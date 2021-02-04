7b8d5c8cf7f67b56ef51d05e921364b63a60771a
gg134679852 <metal852794613@gmail.com>
2021-02-04 22:35:43 +0800

    feat: category filter & edit button

--- public/javascripts/categoryRandom.js
+++ public/javascripts/categoryRandom.js
@@ -1,11 +1,13 @@
 const categoryJson = require('../../models/seeds/category.json').results
 const categoryArry = []
 let categoryIcon = []
+let categoryCh_name = []
 const categoryName = [{ 
-home: ['繳房租', '繳電費', '繳水費', '買家具', '買廚具'] },{transportation: ['儲值悠遊卡']},
-  { entertainment: ['看電影', '買遊戲', '買書', '跟朋友出去玩', '買模型']},
-  { food: ['早餐', '午餐', '晚餐', '點心', '宵夜']},
-  { other: ['購買線上課程', '補習費', '文具', '看醫生', '修電腦']}
+home: ['繳房租', '繳電費', '繳水費', '買家具', '買廚具'],transportation: ['儲值悠遊卡'],
+entertainment: ['看電影', '買遊戲', '買書', '跟朋友出去玩', '買模型'],
+food: ['早餐', '午餐', '晚餐', '點心', '宵夜'],
+other: ['購買線上課程', '補習費', '文具', '看醫生', '修電腦']
+}
 ]
 
 categoryArry.push(...categoryJson)
@@ -20,4 +22,9 @@ while (true) {
   }
 }
 
- module.exports = { categoryIcon, categoryName }
+categoryArry.forEach(element => {
+  categoryCh_name.push(element.name_ch)
+})
+
+
+ module.exports = { categoryIcon, categoryName, categoryCh_name}
